e5cd834a01683d201d0db0b31b31b47e98121636
mannya02 <msoni_be23@thapar.edu>
2026-06-25 05:17:43 +0530

    fix CORS for production

--- backend/main.py
+++ backend/main.py
@@ -33,8 +33,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ "http://localhost:5173",
-        "https://locallancefrontend.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
